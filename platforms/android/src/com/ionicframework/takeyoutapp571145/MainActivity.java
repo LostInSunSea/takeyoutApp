462ce4bb44b0a31366d189fdcfa2ebef5aa37b46
etienne212 <austinsunc@yahoo.com>
2015-08-09 11:12:54 -0700

    "New Files"

--- platforms/android/src/com/ionicframework/takeyoutapp571145/MainActivity.java
+++ platforms/android/src/com/ionicframework/takeyoutapp571145/MainActivity.java
@@ -17,7 +17,7 @@
        under the License.
  */
 
-package com.ionicframework.takeyoutapp133295;
+package com.ionicframework.takeyoutapp571145;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -3,17 +3,55 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            if(window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if(window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    });
+
+    app.config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/connect')
+        $stateProvider.state('connect', {
+            url: '/connect',
+            views: {
+                connect: {
+                    templateUrl: 'views/connectTemplate.html'//,
+                    //controller:'ConnectController'
+                }
+            }
+        })
+        $stateProvider.state('calendar', {
+            url: '/calendar',
+            views: {
+                calendar: {
+                    templateUrl: 'views/calendarTemplate.html'//,
+                    //controller:'CalendarController'
+                }
+            }
+        })
+        $stateProvider.state('message', {
+            url: '/message',
+            views: {
+                message: {
+                    templateUrl: 'views/messageTemplate.html'//,
+                    //controller:'MessageController'
+                }
+            }
+        })
+        $stateProvider.state('profile', {
+            url: '/profile',
+            views: {
+                profile: {
+                    templateUrl: 'views/profileTemplate.html'//,
+                    //controller:'ProfileController'
+                }
+            }
+        })
+    })
