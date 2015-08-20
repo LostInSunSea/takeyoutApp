3a8b84f46e7e298cf5da11d3a7e1a082fb0c9f57
Joanne Arboleda <jbarboleda@ucdavis.edu>
2015-08-20 10:01:57 -0700

    Put Profile Setup on a separate html file, more uniform now with Add Trip. Textarea stops expanding after a specific height.

--- www/js/textarea.js
+++ www/js/textarea.js
@@ -1,11 +1,16 @@
-function resizeTextArea(id){
+function resizeTextArea(id, page){
 	// Reset the height
 	document.getElementById(id).style.height = "";
 	
 	// Get the size of the input in pixels
 	var height = document.getElementById(id).scrollHeight;
-    console.log(height);
 	
-	// Set the height of the textarea 
-	document.getElementById(id).style.height = (height) + "px";
+	// Max height = 100px
+	if(height < 100){
+		// Set the height of the textarea 
+		document.getElementById(id).style.height = (height) + "px";
+	}
+	else{
+		document.getElementById(id).style.height = 100 + "px";
+	}
 }
